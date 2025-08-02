b6b527f84e64b8f844c8d45fc51f60dd5474a504
sesankm <sesank.mallikarjuna@gmail.com>
2025-08-02 15:02:58 -0500

    fix particles overshooting window boundaries

--- main.cpp
+++ main.cpp
@@ -3,9 +3,10 @@
 #include <cstdlib>
 #include <ctime>
 
-const float kGravity = 0.0001;
-const int kWinWidth  = 800;
-const int kWinHeight = 600;
+const float kGravity    = 0.0001;
+const int kParticleMaxX = 600;
+const int kParticleMaxY = 300;
+const int kParticleMinX = 200;
 
 class Particle {
 public:
@@ -19,37 +20,48 @@ public:
     Particle() {
         radius = 8.0f;
         position = sf::Vector2f(388.0f, 100.0f);
-        velocity = sf::Vector2f((rand() % 50 + 1) / 1000.0f, kGravity);
+        velocity = sf::Vector2f((rand() % 100 - 50) / 1000.0f, kGravity);
         bounce_dampner = 1.0;
         roll_dampner = 1.0;
 
         shape = sf::CircleShape(radius);
-        shape.setFillColor(sf::Color(rand() % 155 + 100, rand() % 155 + 100, 255));
+        shape.setFillColor(sf::Color(255, rand() % 155 + 100, rand() % 155 + 100));
         shape.setPosition(position);
     }
 
     void update_vy() {
-        if(position.y + velocity.y <= kWinHeight - (2 * radius)) {
+        if(position.y + velocity.y <= kParticleMaxY - (2 * radius)) {
             velocity.y += kGravity;
-        } else if(position.y >= kWinHeight - (2 * radius)) {
+        } else if(position.y >= kParticleMaxY - (2 * radius)) {
             velocity.y = 0;
-        } else if(velocity.y >= 0) {
-            velocity.y *= -1 * bounce_dampner;
-            if(bounce_dampner > 0) {
-                bounce_dampner -= 0.1;
-            }
         }
+        // else if(velocity.y >= 0) {
+        //     velocity.y *= -1 * bounce_dampner;
+        //     if(bounce_dampner > 0) {
+        //         bounce_dampner -= 0.1;
+        //     }
+        // }
     }
 
     void update_vx() {
-        if(position.x + velocity.x >= kWinWidth - (2 * radius) || position.x + velocity.x <= 0) {
+        if(position.x + velocity.x >= kParticleMaxX - (2 * radius) || position.x + velocity.x < kParticleMinX) {
             velocity.x *= -1;
         }
-        if(position.y >= kWinHeight - (2 * radius)) {
-            if(roll_dampner > 0) {
-                roll_dampner -= 0.00000003;
-                velocity.x *= roll_dampner;
+        // if(position.y >= kParticleMaxY - (2 * radius)) {
+        //     if(roll_dampner > 0) {
+        //         roll_dampner -= 0.00000003;
+        //         velocity.x *= roll_dampner;
+        //     }
+        // }
+    }
+
+    void update_color(int tick) {
+        if(tick % 25 == 0) {
+            sf::Color color = shape.getFillColor();
+            if(color.r > 0) {
+                color.r -= 1;
             }
+            shape.setFillColor(color);
         }
     }
 
@@ -69,23 +81,21 @@ public:
 void check_collision(Particle& p1, Particle& p2) {
     sf::Vector2f diff = p1.position - p2.position;
     float distance = std::sqrt(diff.x * diff.x + diff.y * diff.y);
-    if(distance <= p1.radius + p2.radius) {
+    if(distance < p1.radius + p2.radius) {
         sf::Vector2f norm = (diff / (distance == 0.0f ? 0.0001f : distance));
         float delta = ((p1.radius - p2.radius) * 2) - distance;
-        p1.position += norm;
-        p2.position -= norm;
+
+        sf::Vector2f p1_norm = norm;
+        sf::Vector2f p2_norm = norm;
+        p1.position += p1_norm;
+        p2.position -= p2_norm;
     }
 }
 
 int main() {
     srand(time(0));
     sf::RenderWindow window(sf::VideoMode({800, 600}), "SFML window");
-
     std::vector<Particle> p;
-    p.push_back(Particle());
-    p.push_back(Particle());
-    p.push_back(Particle());
-
     int tick;
 
     while (window.isOpen()) {
@@ -98,20 +108,22 @@ int main() {
 
         window.clear();
         if(tick == 25) {
-            for(int i = 0; i < p.size(); i++) {
-                for(int j = 0; j < p.size(); j++) {
-                    if(i != j) {
-                        check_collision(p[i], p[j]);
-                    }
-                }
-            }
             if(p.size() < 500)
                 p.push_back(Particle());
             tick = 0;
         }
 
         for(int i = 0; i < p.size(); i++) {
+            for(int j = 0; j < p.size(); j++) {
+                if(i != j) {
+                    check_collision(p[i], p[j]);
+                }
+            }
+        }
+
+        for(int i = 0; i < p.size(); i++) {
             p[i].render(window);
+            p[i].update_color(tick);
         }
         window.display();
     }
