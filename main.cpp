971b0979d68ae29bf25afab317c83be0f6b55e96
sesankm <sesank.mallikarjuna@gmail.com>
2025-08-02 12:45:20 -0500

    collision detection

--- main.cpp
+++ main.cpp
@@ -24,7 +24,7 @@ public:
         roll_dampner = 1.0;
 
         shape = sf::CircleShape(radius);
-        shape.setFillColor(sf::Color(255, 255, 255));
+        shape.setFillColor(sf::Color(rand() % 155 + 100, rand() % 155 + 100, 255));
         shape.setPosition(position);
     }
 
@@ -59,19 +59,34 @@ public:
     }
 
     void render(sf::RenderWindow& window) {
-        update();
         window.draw(shape);
     }
 };
 
+void check_collision(Particle& p1, Particle& p2) {
+    sf::Vector2f diff = p1.position - p2.position;
+    float distance = std::sqrt(diff.x * diff.x + diff.y * diff.y);
+    if(distance <= 8.0f) {
+        sf::Vector2f norm = (diff / (distance == 0.0f ? 0.0001f : distance));
+        float delta = (8.0f * 2) - distance;
+        norm *= (delta * 0.002f);
+        p1.velocity += norm;
+        p2.velocity -= norm;
+    }
+}
+
 int main() {
     srand(time(0));
     sf::RenderWindow window(sf::VideoMode({800, 600}), "SFML window");
-
     Particle p;
     Particle p2;
     Particle p3;
+    int tick;
+
+    std::vector<Particle> particles;
+
     while (window.isOpen()) {
+        tick++;
         while (const std::optional event = window.pollEvent()) {
             if (event->is<sf::Event::Closed>()) {
                 window.close();
@@ -79,6 +94,16 @@ int main() {
         }
 
         window.clear();
+
+        if(tick == 25) {
+            check_collision(p, p2);
+            check_collision(p, p3);
+            check_collision(p2, p3);
+            tick = 0;
+        }
+        p.update();
+        p2.update();
+        p3.update();
         p.render(window);
         p2.render(window);
         p3.render(window);
