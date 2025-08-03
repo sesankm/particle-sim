32e7cb218058b1ffab767b2982e5ba03647a2723
sesankm <sesank.mallikarjuna@gmail.com>
2025-08-03 18:05:05 -0500

    switch to verlet integration instead of euler method for easier boundary handling

--- main.cpp
+++ main.cpp
@@ -3,113 +3,115 @@
 #include <cstdlib>
 #include <ctime>
 
-const float kGravity    = 3.0f;
-const int kParticleMaxX = 800;
-const int kParticleMaxY = 600;
-const int kParticleMinX = 0;
+const int   WIN_H    = 800;
+const int   WIN_W    = 800;
+const sf::Vector2f GRAV(0.0f, 9.8f);
 
 class Particle {
 public:
     float radius;
-    sf::Vector2f position;
-    sf::Vector2f velocity;
+    sf::Vector2f pos;
+    sf::Vector2f prev_pos;
+    sf::Vector2f accel;
+
     sf::CircleShape shape;
-    float bounce_dampner;
-    float roll_dampner;
 
     Particle() {
-        radius = 10.0f;
-        position = sf::Vector2f(388.0f, 200.0f);
-        velocity = sf::Vector2f((rand() % 100 - 50) / 1000.0f, kGravity);
-        bounce_dampner = 2.0;
-        roll_dampner = 1.0;
+        radius = 40.0f;
+
+        accel = {};
+        pos = sf::Vector2f(rand() % 40 + 340, 200.0f);
+        prev_pos = pos;
 
         shape = sf::CircleShape(radius);
-        shape.setFillColor(sf::Color(255, rand() % 155 + 100, rand() % 155 + 100));
-        shape.setPosition(position);
+        shape.setFillColor(sf::Color(255, 255, 255));
+        shape.setPosition(pos);
     }
 
-    void update_vy() {
-        if(position.y + radius > kParticleMaxY) {
-            velocity.y = -2.0f * bounce_dampner;
-        } else {
-            if(velocity.y < 0) {
-                velocity.y += 0.10;
-            } else if(position.y + velocity.y <= kParticleMaxY - radius) {
-                velocity.y += kGravity / (radius * radius);
-            }
+    void apply_grav() {
+        accel += GRAV;
+    }
+
+    void update_pos(float dt) {
+        sf::Vector2f vel = prev_pos - pos;
+        prev_pos = pos;
+        pos = pos + vel + accel * (dt * dt);
+        accel = {};
+    }
+
+    void check_boundary() {
+        if(pos.y + radius * 2 > WIN_H) {
+            pos.y -= (pos.y + radius * 2 - WIN_H);
         }
-    }
-
-    void update_vx() {
-        if(position.x + velocity.x >= kParticleMaxX - (2 * radius) || position.x + velocity.x < kParticleMinX) {
-            velocity.x *= -1;
+        if(pos.x < 0) {
+            pos.x += (0 - pos.x);
+        } else if(pos.x + radius * 2 > WIN_W) {
+            pos.x -= (pos.x + radius * 2 - WIN_W);
         }
-    }
-
-    void update_color() {
-        sf::Color color = shape.getFillColor();
-        if(color.r > 0) {
-            color.r -= 1;
-        }
-        shape.setFillColor(color);
-    }
-
-    void update() {
-        update_vy();
-        update_vx();
-        position += velocity;
-        shape.setPosition(position);
     }
 
     void render(sf::RenderWindow& window) {
-        update();
+        shape.setPosition(pos);
         window.draw(shape);
     }
 };
 
 void check_collision(Particle& p1, Particle& p2) {
-    sf::Vector2f diff = p1.position - p2.position;
-    float distance = std::sqrt(diff.x * diff.x + diff.y * diff.y);
-    if(distance < p1.radius + p2.radius) {
-        sf::Vector2f norm = (diff / (distance == 0.0f ? 0.0001f : distance));
-        float delta = ((p1.radius - p2.radius) * 2) - distance;
-        p1.position += norm;
-        p2.position -= norm;
+    sf::Vector2f diff = p1.pos - p2.pos;
+    float dist = std::sqrt(diff.x * diff.x + diff.y * diff.y);
+
+    if(dist < p1.radius + p2.radius) {
+        sf::Vector2f norm = diff / dist;
+        float delta = p1.radius + p2.radius - dist;
+        p1.pos += 0.5f * delta * norm;
+        p2.pos -= 0.5f * delta * norm;
+    }
+}
+
+
+void update_positions(std::vector<Particle>& particles) {
+    int substeps = 2;
+    const float sub_dt = 1.0f / static_cast<float>(substeps);
+
+    for(int s = 0; s < substeps; s++) {
+        for(int i = 0; i < particles.size(); i++) {
+            particles[i].apply_grav();
+            particles[i].update_pos(sub_dt);
+            for(int j = 0; j < particles.size(); j++) {
+                if(i != j) {
+                    check_collision(particles[i], particles[j]);
+                }
+            }
+            particles[i].check_boundary();
+        }
     }
 }
 
 int main() {
     srand(time(0));
-    sf::RenderWindow window(sf::VideoMode({800, 600}), "SFML window");
+    sf::RenderWindow window(sf::VideoMode({WIN_W, WIN_H}), "SFML window");
     std::vector<Particle> p;
-    window.setFramerateLimit(120);
-    int tick;
+
+    p.push_back(Particle());
+    window.setFramerateLimit(240);
 
     while (window.isOpen()) {
-        tick++;
         while (const std::optional event = window.pollEvent()) {
             if (event->is<sf::Event::Closed>()) {
                 window.close();
             }
-        }
-
-        window.clear();
-        if(p.size() < 1000)
-            p.push_back(Particle());
-
-        for(int i = 0; i < p.size(); i++) {
-            for(int j = 0; j < p.size(); j++) {
-                if(i != j) {
-                    check_collision(p[i], p[j]);
+            if (const auto* keyPressed = event->getIf<sf::Event::KeyPressed>()) {
+                if (keyPressed->scancode == sf::Keyboard::Scan::A) {
+                    p.push_back(Particle());
                 }
             }
         }
 
-        for(int i = 0; i < p.size(); i++) {
-            p[i].render(window);
-            p[i].update_color();
-        }
+        window.clear();
+        update_positions(p);
+        std::for_each(p.begin(), p.end(), [&window](Particle& pa) {
+            pa.render(window);
+        });
         window.display();
     }
     return 0;
