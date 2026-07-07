31d44eea6990b2e96db25710d06eda18b8981abe
sesankm <26676400+sesankm@users.noreply.github.com>
2026-07-06 20:51:31 -0500

    bump N_TS up to 4 after optimizations; increase num particles; set cell width based on particle radius

--- include/constants.hpp
+++ include/constants.hpp
@@ -3,14 +3,14 @@
 
 const int   WIN_W     = 800;
 const int   WIN_H     = 600;
-const int   CELL_W    = 20;
-const int   N_PARTS   = 10000;
-const int   N_TS      = 2.0;
+const int   N_PARTS   = 20000;
+const int   N_TS      = 4.0;
 const int   N_THREADS = 5;
-const float PART_R    = 5;
 const float G_FORCE   = 0.098;
 
-constexpr int GRID_ROWS = WIN_H / CELL_W;
-constexpr int GRID_COLS = WIN_W / CELL_W;
+constexpr float PART_R    = 3;
+constexpr int   CELL_W    = PART_R * 4;
+constexpr int   GRID_ROWS = WIN_H / CELL_W;
+constexpr int   GRID_COLS = WIN_W / CELL_W;
 
 #endif
