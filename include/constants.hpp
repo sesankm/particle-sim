3bfc27b82f9a9b4c414d29cd7ad66b234eb08552
sesankm <26676400+sesankm@users.noreply.github.com>
2026-07-10 13:20:48 -0500

    update constants

--- include/constants.hpp
+++ include/constants.hpp
@@ -1,11 +1,13 @@
 #ifndef CONSTANTS_H
 #define CONSTANTS_H
 
+// const int   WIN_W     = 1000; 
+// const int   WIN_H     = 700;
 const int   WIN_W     = 800;
 const int   WIN_H     = 600;
 const int   N_PARTS   = 20000;
-const int   N_TS      = 4.0;
-const int   N_THREADS = 4;
+const int   N_TS      = 3.0;
+const int   N_THREADS = 10;
 const float G_FORCE   = 0.098;
 
 constexpr float PART_R    = 3;
@@ -14,3 +16,4 @@ constexpr int   GRID_ROWS = WIN_H / CELL_W;
 constexpr int   GRID_COLS = WIN_W / CELL_W;
 
 #endif
+
